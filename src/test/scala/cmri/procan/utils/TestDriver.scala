08b0219889a94dffbe25969b3461b24888e7d884
Max Wittmann <damxam@gmail.com>
2019-05-22 17:17:56 +1000

    Cleanup

--- src/test/scala/cmri/procan/utils/TestDriver.scala
+++ src/test/scala/cmri/procan/utils/TestDriver.scala
@@ -4,15 +4,14 @@ import org.neo4j.driver.v1._
 
 import cmri.procan.checkgraph.utils.WrappedNeo4jDriver
 
+// Todo: uri and auth should come from config
 object TestDriver {
   private val token: AuthToken = AuthTokens.basic("neo4j", "test")
 
   val driver: Driver = GraphDatabase.driver(
     "bolt://127.0.0.1:7687",
     token,
-    Config.build
-      .withEncryptionLevel(Config.EncryptionLevel.NONE)
-      .toConfig
+    Config.build.toConfig
   )
 
   val wrappedDriver = new WrappedNeo4jDriver(driver)
